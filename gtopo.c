185e5ae1d7c5750f25ad6e502c53fe3f24c40201
Tom Trebisky <tom@mmto.org>
2007-07-06 18:50:33 +0000

    gtopo 0.4

--- gtopo.c
+++ gtopo.c
@@ -14,9 +14,18 @@
  *	the drawing area on an expose event.  7/3/2007
  * version 0.3 - actually pull a single maplet out of
  *	a TPQ file and display it.  7/5/2007
+ * version 0.4 - pull a 2x2 group of maplets out of
+ *	a TPQ file and display them.  7/5/2007
  */
 
+/* This one has 50 jpeg maplets in a 5x10 pattern */
 char *tpq_file = "../q36117h8.tpq";
+
+#ifdef notdef
+/* This one has 100 jpeg maplets in a 10x10 pattern */
+/*  each is 406x480 */
+char *tpq_file = "../F30105A1.TPQ";
+#endif
 
 GdkColormap *syscm;
 
@@ -25,13 +34,13 @@ GtkWidget *main_da;
 /* A pixmap backup of the drawing area */
 GdkPixmap *main_pixels = NULL;
 
-/* One maplet (will change) */
-GdkPixbuf *map_buf;
+/* The maplets */
+GdkPixbuf *map_buf[4];
 
 /* Prototypes ..........
  */
 void build_tpq_index ( char * );
-GdkPixbuf *load_tpq_maplet ( char *name, int who );
+GdkPixbuf *load_tpq_maplet ( char *, int, int );
 
 void
 error ( char *msg, char *arg )
@@ -52,8 +61,10 @@ static int expose_count = 0;
 gint
 expose_handler ( GtkWidget *wp, GdkEventExpose *ep )
 {
+	/*
 	if ( expose_count < 4 )
 	    printf ( "Expose event %d\n", expose_count++ );
+	    */
 
 	gdk_draw_pixmap ( wp->window,
 		wp->style->fg_gc[GTK_WIDGET_STATE(wp)],
@@ -80,14 +91,11 @@ static int config_count = 0;
 #define SRC_X	0
 #define SRC_Y	0
 
-#define DEST_X	0
-#define DEST_Y	0
-
 void
-draw_maplet ( GdkPixbuf *map )
+draw_maplet ( GdkPixbuf *map, int x, int y )
 {
 	gdk_draw_pixbuf ( main_pixels, NULL, map,
-		SRC_X, SRC_Y, DEST_X, DEST_Y, -1, -1,
+		SRC_X, SRC_Y, x, y, -1, -1,
 		GDK_RGB_DITHER_NONE, 0, 0 );
 }
 
@@ -95,6 +103,7 @@ gint
 configure_handler ( GtkWidget *wp, gpointer data )
 {
 	int w, h;
+	int mw, mh;
 
 	w = wp->allocation.width;
 	h = wp->allocation.height;
@@ -110,7 +119,17 @@ configure_handler ( GtkWidget *wp, gpointer data )
 	/* clear the whole pixmap to white */
 	gdk_draw_rectangle ( main_pixels, wp->style->white_gc, TRUE, 0, 0, w, h );
 
-	draw_maplet ( map_buf );
+	mw = gdk_pixbuf_get_width ( map_buf[0] );
+	mh = gdk_pixbuf_get_height ( map_buf[0] );
+	/*
+	*/
+
+	/*
+	*/
+	draw_maplet ( map_buf[0], 0, 0 );
+	draw_maplet ( map_buf[1], mw, 0 );
+	draw_maplet ( map_buf[2], 0, mh );
+	draw_maplet ( map_buf[3], mw, mh );
 
 	return TRUE;
 }
@@ -146,17 +165,25 @@ main ( int argc, char **argv )
 	syscm = gdk_colormap_get_system ();
 
 	/* maplet 2 is the old topo0301.jpg */
-	map_buf = load_tpq_maplet ( tpq_file, 2 );
+	/* This block of 4 will be:
+		2  3
+		7  8
+	*/
+	map_buf[0] = load_tpq_maplet ( tpq_file, 2, 0 );
+	map_buf[1] = load_tpq_maplet ( tpq_file, 3, 0 );
 
-	w = gdk_pixbuf_get_width ( map_buf );
-	h = gdk_pixbuf_get_height ( map_buf );
+	map_buf[2] = load_tpq_maplet ( tpq_file, 2, 1 );
+	map_buf[3] = load_tpq_maplet ( tpq_file, 3, 1 );
 
-	/* initially big enough for one maplet */
-	gtk_drawing_area_size ( GTK_DRAWING_AREA(main_da), w, h );
+	w = gdk_pixbuf_get_width ( map_buf[0] );
+	h = gdk_pixbuf_get_height ( map_buf[0] );
+
+	/* big enough for four maplets */
+	gtk_drawing_area_size ( GTK_DRAWING_AREA(main_da), w*2, h*2 );
 
 	gtk_widget_show ( main_da );
 
-	printf ( "%d by %d\n", w, h );
+	printf ( "single maplet size: %d by %d\n", w, h );
 
 	gtk_main ();
 
@@ -172,36 +199,66 @@ main ( int argc, char **argv )
 /* We really see just 50 in the TPQ files we process,
  * but allocate a bit more room, for no particular reason.
  */
-#define TPQ_NUM_MAPLETS	64
+#define TPQ_MAX_MAPLETS	200
 
 struct tpq_index_e {
 	long	offset;
 	long	size;
 };
 
-struct tpq_index_e tpq_index[TPQ_NUM_MAPLETS];
+struct tpq_index_e tpq_index[TPQ_MAX_MAPLETS];
 char tpq_index_filename[100];
 int num_index = 0;
 
-/* For a TPQ file representing a 7.5 minute quadrangle, there
- * are 50 maplets within the file.  These begin in the upper
+/* For a q-series TPQ file representing a 7.5 minute quadrangle,
+ * there are 50 maplets within the file.  These begin in the upper
  * left (the far NW) and then move W to E for 5 maplets
  * then down just like you read lines of text in a book.
  * There are 10 maplets N to S
+ *
+ * For a F series TPQ file representing a 5x5 degree area,
+ * there are 100 maplets in the file in a 10x10 pattern,
+ * the same order as above.
  */
 
 #define BUFSIZE	1024
 
+#ifdef BIG_ENDIAN_HACK
+#define JPEG_SOI_TAG	0xffd8
+#else
+#define JPEG_SOI_TAG	0xd8ff
+#endif
+
 void
-wonk_index ( long *info )
+wonk_index ( int fd, long *info )
 {
 	int i;
+	unsigned short tag;
+	int num_jpeg;
 
-	num_index = (info[0] - 1024) - 4;
+	num_index = (info[0] - 1024)/4 - 4;
+	/*
+	printf ( "table entries: %d\n", num_index );
+	*/
+	if ( num_index > TPQ_MAX_MAPLETS )
+	    error ("Whoa! too many maplets\n", "" );
+
+	num_jpeg = 0;
 	for ( i=0; i<num_index; i++ ) {
 	    tpq_index[i].offset = info[i];
 	    tpq_index[i].size = info[i+1] - info[i];
+	    lseek ( fd, info[i], SEEK_SET );
+	    if ( read( fd, &tag, sizeof(tag) ) != sizeof(tag) )
+		error ( "tag read fails!\n", "" );
+	    if ( tag != JPEG_SOI_TAG )
+		break;
+	    num_jpeg++;
 	}
+
+	num_index = num_jpeg;
+	/*
+	printf ( "jpeg table entries: %d\n", num_index );
+	*/
 }
 
 void
@@ -209,6 +266,9 @@ build_tpq_index ( char *name )
 {
 	int fd;
 	char buf[BUFSIZE];
+
+	if ( num_index > 0 && strcmp ( tpq_index_filename, name ) == 0 )
+	    return;
 
 	fd = open ( name, O_RDONLY );
 	if ( fd < 0 )
@@ -220,7 +280,7 @@ build_tpq_index ( char *name )
 	if ( read( fd, buf, BUFSIZE ) != BUFSIZE )
 	    error ( "Bogus TPQ file size - 2\n", "" );
 
-	wonk_index ( (long *) buf );
+	wonk_index ( fd, (long *) buf );
 	strcpy ( tpq_index_filename, name );
 
 	close ( fd );
@@ -232,18 +292,23 @@ char *tmpname = "gtopo.tmp";
  * returns NULL if trouble.
  */
 GdkPixbuf *
-load_tpq_maplet ( char *name, int who )
+load_tpq_maplet ( char *name, int x_index, int y_index )
 {
 	char buf[BUFSIZE];
 	int fd, ofd;
 	int size;
 	int nw;
+	int nlong;
+	int who;
 	GdkPixbuf *rv;
 
-	if ( num_index < 1 )
-	    build_tpq_index ( name );
-	if ( strcmp ( tpq_index_filename, name ) != 0 )
-	    build_tpq_index ( name );
+	build_tpq_index ( name );
+
+	nlong = 5;
+	if ( num_index == 100 )
+	    nlong = 10;
+
+	who = y_index * nlong + x_index;
 
 	/* open a temp file for R/W */
 	ofd = open ( tmpname, O_CREAT | O_TRUNC | O_WRONLY, 0600 );
